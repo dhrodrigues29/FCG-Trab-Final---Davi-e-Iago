716dd4892ac3b08e109686c9edc32a99c680a62d
Davi Haas <davihaasrodrigues@gmail.com>
2022-10-07 13:21:46 -0300

    add walls and texture to it

--- src/main.cpp
+++ src/main.cpp
@@ -272,22 +272,22 @@ void RenderPlane()
 void RenderWalls()
 {
     glm::mat4 model = Matrix_Identity();
-    model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Rotate_X((PI*3)/2) * Matrix_Translate(0.0f,-0.99f,0.0f);
-    glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
-    glUniform1i(object_id_uniform, WALL);
-    DrawVirtualObject("plane");
-
-    model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Rotate_X(PI/2) * Matrix_Translate(0.0f,-0.99f,0.0f);
-    glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
-    glUniform1i(object_id_uniform, WALL);
-    DrawVirtualObject("plane");
-
     model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Translate(0.0f,-0.99f,0.0f);
     glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
     glUniform1i(object_id_uniform, WALL);
     DrawVirtualObject("plane");
 
-    model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Rotate_X(PI) * Matrix_Translate(0.0f,-0.99f,0.0f);
+    model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Rotate_Z(PI/2) * Matrix_Translate(0.0f,-0.99f,0.0f);
+    glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
+    glUniform1i(object_id_uniform, WALL);
+    DrawVirtualObject("plane");
+
+    model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Rotate_Z(PI) * Matrix_Translate(0.0f,-0.99f,0.0f);
+    glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
+    glUniform1i(object_id_uniform, WALL);
+    DrawVirtualObject("plane");
+
+    model = Matrix_Scale(PLANE_SIZE,1.0f,PLANE_SIZE) * Matrix_Rotate_X(PI/2) * Matrix_Rotate_Z((PI*3)/2) * Matrix_Translate(0.0f,-0.99f,0.0f);
     glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
     glUniform1i(object_id_uniform, WALL);
     DrawVirtualObject("plane");
@@ -660,6 +660,7 @@ int main(int argc, char *argv[])
 
         }
 
+        RenderWalls();
         RenderPlane();
         TextRendering_ShowFramesPerSecond(window, remainingBunnys);
         glfwSwapBuffers(window);
